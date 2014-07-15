3cf0f10a5f52b79f65194d8fd3a7b1bd000e1c36
David Asabina <david@supr.nu>
2014-07-15 21:30:45 +0200

    Exposing the connection object all resource instances and the Model

--- lib/muve.rb
+++ lib/muve.rb
@@ -6,4 +6,18 @@ module Muve
   require "muve/location"
   require "muve/traveller"
   require "muve/movement"
+
+  module Model
+    def connection
+      Model.connection
+    end
+
+    def self.connection
+      @@conn ||= Object.new
+    end
+  end
+
+  def self.init
+    Model.connection
+  end
 end
