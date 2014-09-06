0ea21bf38636ef4f82cb9c1b9f7098ba0d175632
David Asabina <david@supr.nu>
2014-09-06 14:44:23 +0200

    Making a movement aware of its location-related attributes

--- lib/muve/movement.rb
+++ lib/muve/movement.rb
@@ -4,6 +4,33 @@ module Muve
   
     with_fields :traveller, :location, :time
   
+    alias_method :lat,   :latitude
+    alias_method :lat=,  :latitude=
+    alias_method :lon,   :longitude
+    alias_method :lon=,  :longitude=
+    alias_method :lng,   :longitude
+    alias_method :lng=,  :longitude=
+    alias_method :long,  :longitude
+    alias_method :long=, :longitude=
+
+    def latitude
+      location[:latitude] if location
+    end
+
+    def longitude
+      location[:longitude] if location
+    end
+
+    def latitude=(value)
+      location = {} unless location
+      location[:latitude]=(value) 
+    end
+
+    def longitude=(value)
+      location = {} unless location
+      location[:longitude]=(value) 
+    end
+
     def valid?
       assocs.each do |assoc|
         return false unless !assoc.nil? && assoc.valid?
