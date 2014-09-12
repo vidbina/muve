f5045e063964be673b4d1ef1530e69f14bd721f1
David Asabina <david@supr.nu>
2014-09-13 01:40:28 +0200

    Set-up invalid traits for factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,20 +2,37 @@ FactoryGirl.define do
   factory Muve::Location do
     lat { Faker::Geolocation.lat }
     long { Faker::Geolocation.lng }
+
+    trait :invalid do
+      lat { 200 }
+      long { 200 }
+    end
   end
 
   factory Muve::Traveller do
     name { Faker::Name.name }
+
+    trait :invalid do
+      name { nil }
+    end
   end
 
   factory Muve::Movement do
     traveller { build Muve::Traveller }
     location { build Muve::Location }
     time { Time.now - rand(500000) }
+
+    trait :invalid do
+      location { build Muve::Location, :invalid }
+    end
   end
 
   factory Muve::Place do
     name { Faker::Venue.name }
     location { build Muve::Location }
+
+    trait :invalid do
+      location { build Muve::Location, :invalid }
+    end
   end
 end
